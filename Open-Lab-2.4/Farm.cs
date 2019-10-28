e4705960cab6d49ece48ca9a2957be43cf3554ab
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-28 08:46:45 +0100

    Add project files.

---
+++ Open-Lab-2.4/Farm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_2._4
+{
+    class Farm
+    {
+        public int GetLegsCount(int chickens, int cows, int pigs)
+        {
+            int getchickenlegs = chickens * 2;
+            int getcowlegs = cows * 4;
+            int getpiglegs = pigs * 4;
+            return getchickenlegs + getcowlegs + getpiglegs;
+        }
+    }
+}
---
+++ Open-Lab-2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farm legs = new Farm();
+            Console.WriteLine(Convert.ToInt32(legs.GetLegsCount(2, 1, 7)));
+        }
+    }
+}
